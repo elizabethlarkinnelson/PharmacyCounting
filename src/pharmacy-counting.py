eaca191e01dbc1d599ce62a98b2546d3a1aec04d
Elizabeth Nelson <ENelson@travisusd.org>
2019-01-31 15:42:05 -0800

    Parsing file logic ip

--- src/pharmacy-counting.py
+++ src/pharmacy-counting.py
@@ -4,5 +4,11 @@ data_folder = join(dirname(dirname(abspath(__file__))), "input")
 
 file_to_open = join(data_folder, "icont.txt")
 
-f = open(file_to_open)
 
+drugs = {}
+
+with open(file_to_open) as f:
+	next(f)
+	for line in f:
+		line = line.strip()
+		print(line)
