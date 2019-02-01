df4e70346e80e6f0157dc99a22d536d37e398643
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2019-01-31 21:52:30 -0800

    Writes output file, not ordered

--- src/pharmacy-counting.py
+++ src/pharmacy-counting.py
@@ -5,7 +5,9 @@ data_folder = join(dirname(dirname(abspath(__file__))), "input")
 file_to_open = join(data_folder, "icont.txt")
 
 
-drugs = {}
+drug_providers = {}
+
+total_cost = {}
 
 
 def listify_line(line):
@@ -17,11 +19,20 @@ with open(file_to_open) as f:
         line = line.strip()
         line = listify_line(line)
         provider = line[1] + " " + line[2]
-        drug = (line[3], line[4])
-        if drug not in drugs:
-            drugs[drug] = set([provider])
+        drug = line[3]
+        cost = int(line[4])
+        if drug not in drug_providers:
+            drug_providers[drug] = set([provider])
+            total_cost[drug] = cost
         else:
-            #set.add() doesn't add to set if already there
-            drugs[drug].add(provider)
+            drug_providers[drug].add(provider)
+            total_cost[drug] += cost
 
-print(drugs)
+output_data_folder = join(dirname(dirname(abspath(__file__))), "output")
+
+with open(join(output_data_folder, 'top_drug_cost.txt'), 'w') as drug_cost:
+    for drug in drug_providers:
+        total_providers = str(len(drug_providers[drug]))
+        drug_cost.write(drug + "," + total_providers + "," + str(total_cost[drug]) + "\n")
+
+
