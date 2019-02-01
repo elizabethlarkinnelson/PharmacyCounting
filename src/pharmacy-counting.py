968b6dac7a73cf58d0855ab65724decd0f1b92c2
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2019-01-31 21:17:52 -0800

    1st iteration of storing drug information as dictionary of tuples

--- src/pharmacy-counting.py
+++ src/pharmacy-counting.py
@@ -7,8 +7,21 @@ file_to_open = join(data_folder, "icont.txt")
 
 drugs = {}
 
+
+def listify_line(line):
+    return line.split(",")
+
 with open(file_to_open) as f:
-	next(f)
-	for line in f:
-		line = line.strip()
-		print(line)
+    next(f)
+    for line in f:
+        line = line.strip()
+        line = listify_line(line)
+        provider = line[1] + " " + line[2]
+        drug = (line[3], line[4])
+        if drug not in drugs:
+            drugs[drug] = set([provider])
+        else:
+            #set.add() doesn't add to set if already there
+            drugs[drug].add(provider)
+
+print(drugs)
