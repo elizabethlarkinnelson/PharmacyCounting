5707b6f1c9f0a8be910685144006038e93d9124d
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2019-02-07 19:35:41 -0800

    Testone's mixed up

--- insight_testsuite/tests/test1/testone.py
+++ insight_testsuite/tests/test1/testone.py
@@ -1,22 +1,30 @@
 import unittest
 from os.path import join, dirname, abspath
+import sys
+
+root_directory = abspath(join(dirname(__file__), '..', '..', '..', 'src'))
+
+print(root_directory)
+sys.path += [root_directory]
 
 
+from pharmacycounting import test
 
 
-# class TestSortingMethods(unittest.TestCase):
+class TestSortingMethods(unittest.TestCase):
 
-#     def test_first_line(self):
-        
-#         # data_folder = join(dirname(dirname(abspath(__file__))), "output")
-#         # file_to_open = join(data_folder, "top_drug_cost.txt")
-#         # with open(file_to_open) as f:
-#         #     f.readline()
-#         #     first_line = f.readline()
-#         # first_drug = "CHLORPROMAZINE,2,3000"
-#         # self.assertEqual(first_line.strip(), first_drug)
+    def test_hello(self):
+        self.assertEqual(test, "hi")
 
-# if __name__ == '__main__':
+    # def test_first_line(self):
+        # data_folder = join(dirname(dirname(abspath(__file__))), "output")
+        # file_to_open = join(data_folder, "top_drug_cost.txt")
+        # with open(file_to_open) as f:
+        #     f.readline()
+        #     first_line = f.readline()
+        # first_drug = "CHLORPROMAZINE,2,3000"
+        # self.assertEqual(first_line.strip(), first_drug)
 
-#     unittest.main()
+if __name__ == '__main__':
 
+    unittest.main()
